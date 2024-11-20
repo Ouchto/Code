8c76e15f45cf4c441f1c0c2df6614d5867c1c286
Ouchto <mustapha.enaa@gmail.com>
2024-11-20 11:34:27 +0100

    Mettre à jour les informations d'une personne en modifiant les champs existants.

--- structure.c
+++ structure.c
@@ -17,11 +17,9 @@ typedef struct
 
 int main(){
     int n;
-    Personne p[n];
-
     printf("Entrez le nombre de personnes : ");
     scanf("%d",&n);
-
+    Personne p[n];
     for(int i = 1; i <= n; i++){
         printf("Entrez le nom du personne  %d : ",i);
         scanf("%s",&p[i].nom);
@@ -37,12 +35,40 @@ int main(){
         
     }
 
+
     for(int i = 1; i <= n; i++){
         printf(" les informations du personne %d : \n",i);
         printf(" le nom : %s  \n l'age :  %d \n l'adresse : \n ==> la rue : %s \n ==> la ville : %s \n ==> le code postal : %d \n",
                 p[i].nom,p[i].age,p[i].address.rue,p[i].address.ville,p[i].address.code_postal);
 
     }
+
+
+    int np;
+    printf("Entrez le nombre de personne a modifier : ");
+    scanf("%d",&np);
+    if (np >= 1 && np >= n)
+    {
+        printf("Entrez le  nom : ");
+        scanf("%s",&p[np].nom);
+        printf("Entrez  l'age du personne : ");
+        scanf("%d",&p[np].age);
+        printf("Entrez l'adresse du personne  : \n");
+        printf("Entrez la rue :");
+        scanf("%s",&p[np].address.rue);
+        printf("Entrez la ville : \n");
+        scanf("%s",&p[np].address.ville);
+        printf("Entrez le code postal :\n ");
+        scanf("%d",&p[np].address.code_postal);
+    }
+    
+    for(int i = 1; i <= n; i++){
+        printf(" les informations du personne %d : \n",i);
+        printf(" le nom : %s  \n l'age :  %d \n l'adresse : \n ==> la rue : %s \n ==> la ville : %s \n ==> le code postal : %d \n",
+                p[i].nom,p[i].age,p[i].address.rue,p[i].address.ville,p[i].address.code_postal);
+
+    }
+    
 
 
     return 0;
