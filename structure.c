23ef2985f2ab7320efbd529a041034dc77165f77
Ouchto <mustapha.enaa@gmail.com>
2024-11-20 14:46:04 +0100

    Supprimer une personne de la liste des personnes créées.

--- structure.c
+++ structure.c
@@ -2,14 +2,14 @@
 
 typedef struct 
 {
-    char rue[100];
-    char ville[100];
+    signed char rue[100];
+    signed char ville[100];
     int code_postal;
 } adresse;
 
 typedef struct 
 {
-    char nom[100];
+    signed char nom[100];
     int age;
     adresse address;
 }Personne;
@@ -47,7 +47,7 @@ int main(){
     int np;
     printf("Entrez le nombre de personne a modifier : ");
     scanf("%d",&np);
-    if (np >= 1 && np >= n)
+    if (np >= 1 && np <= n)
     {
         printf("Entrez le  nom : ");
         scanf("%s",&p[np].nom);
@@ -60,8 +60,9 @@ int main(){
         scanf("%s",&p[np].address.ville);
         printf("Entrez le code postal :\n ");
         scanf("%d",&p[np].address.code_postal);
+    }else{
+        printf("le nombre de personne n'existe pas.");
     }
-    
     for(int i = 1; i <= n; i++){
         printf(" les informations du personne %d : \n",i);
         printf(" le nom : %s  \n l'age :  %d \n l'adresse : \n ==> la rue : %s \n ==> la ville : %s \n ==> le code postal : %d \n",
@@ -69,6 +70,42 @@ int main(){
 
     }
     
+    int nps,i;
+    printf("Entrez le nombre de personne a suppriee : ");
+    scanf("%d",&nps);
+
+
+if ( nps >= 0 && nps <= n ) {
+        // Décaler tous les éléments après la position i d'une case vers la gauche
+        for (int j = nps; j < n - 1; j++) {
+            p[j] = p[j + 1];
+        }
+        n--;  // Réduire le nombre total de personnes
+    } else {
+        printf("Index invalide.\n");
+    }
+
+
+ for(int i = 1; i <= n; i++){
+        printf(" les informations du personne %d : \n",i);
+        printf(" le nom : %s  \n l'age :  %d \n l'adresse : \n ==> la rue : %s \n ==> la ville : %s \n ==> le code postal : %d \n",
+                p[i].nom,p[i].age,p[i].address.rue,p[i].address.ville,p[i].address.code_postal);
+
+    }
+
+
+/*  
+
+    if (nps >= 1 && nps <= n) {
+        
+        printf("le personne a été supprimee");
+
+    }else{
+        printf("le nombre de personne n'existe pas.");
+    }
+
+*/
+
 
 
     return 0;
